91cbdd2ba7358f14562c242dba7ad9dcdf5c0609
Krzysztof Matyga <krzysztof.matyga@noinputsignal.com>
2023-03-28 21:47:17 +0200

    routes added, layout and header components added

--- src/features/posts/PostsExcerpt.js
+++ src/features/posts/PostsExcerpt.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
@@ -7,9 +8,10 @@ import ReactionButtons from "./ReactionButtons";
 const PostsExcerpt = ({ post }) => {
   return (
       <article>
-          <h3>{post.title}</h3>
-          <p>{post.body.substring(0, 100)}</p>
+          <h2>{post.title}</h2>
+          <p className='excerpt'>{post.body.substring(0, 100)}</p>
           <p className="postCredit">
+              <Link to={`post/${post.id}`}>View Post</Link>
               <PostAuthor userId={post.userId} />
               <TimeAgo timestamp={post.date} />
           </p>
